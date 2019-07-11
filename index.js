91723ef2f03967ffee739f52bf7f89e465601f4f
Kevin Sims <kevinsims1@outlook.com>
2019-07-11 13:15:27 -0700

    mvp

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const server = require('./server.js')
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-    console.log(`\n* Server Running on http://localhost:${port} *\n1`)
+    console.log(`\n* Server Running on http://localhost:${port} *\n`)
 })
